9b70f0c52c0cd73f3bdab7a2dd3d4643ad254c17
Augustin <adebureaux@gmail.com>
2021-12-07 20:17:53 +0100

    debug

--- srcs/main.c
+++ srcs/main.c
@@ -10,9 +10,9 @@ int	main(int argc, char **argv)
 
 	gtk_init(&argc, &argv);
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title (GTK_WINDOW (window), "CALCULATEUR DE PRIX");
-	gtk_window_set_default_size (GTK_WINDOW (window), 1200, 300);
-	gtk_window_set_position(GTK_WINDOW (window), GTK_WIN_POS_CENTER);
+	gtk_window_set_title(GTK_WINDOW(window), "CALCULATEUR DE PRIX");
+	gtk_window_set_default_size(GTK_WINDOW(window), 1200, 300);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
 	grid = gtk_grid_new();
